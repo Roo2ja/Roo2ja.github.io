8d8bbf21e1cf33b29e7d68db98ce80c4b84e1b92
Roo2ja <42668597+Roo2ja@users.noreply.github.com>
2018-12-14 17:32:35 +0530

    1st commit

---
+++ precache-manifest.17ae0a9e06f418e88fd9ce93b75c1de0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5b3d758e18e8ec40fd05",
+    "url": "/static/js/main.5b3d758e.chunk.js"
+  },
+  {
+    "revision": "5910d9b836c12b456c1e",
+    "url": "/static/js/1.5910d9b8.chunk.js"
+  },
+  {
+    "revision": "c39d00c469490a75a3ed0fdcec47fc11",
+    "url": "/index.html"
+  }
+];
